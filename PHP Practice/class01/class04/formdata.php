31320bde385e61c48c392b1ad3c3ff57a191775b
Nowrin Sultana Srabony <nowrin.505sultana@gmail.com>
2026-05-20 02:34:49 +0600

    Delete PHP Practice/class01 directory

--- PHP Practice/class01/class04/formdata.php
+++
@@ -1,5 +0,0 @@
-<?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-echo "user name is $name <br>";
-echo "user email is $email <br>";
--- PHP Practice/class01/class06/env.php
+++
@@ -1,9 +0,0 @@
-<!-- crud -- create read update delete -->
-
-<?php
-$dbhostname = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "crud_project";
-
-$conn = mysqli_connect($dbhostname, $dbusername, $dbpassword, $dbname);
