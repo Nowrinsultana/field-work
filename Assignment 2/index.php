4e629a4ea51420c317c74a7033f1db3fc28db448
Nowrinsultana <c213250@ugrad.iiuc.ac.bd>
2026-05-20 18:45:31 +0600

    assignment 2

---
+++ Assignment 2/index.php
@@ -0,0 +1,23 @@
+<?php
+include("./header.php");
+?>
+
+<div class="card col-md-6 w-50 mx-auto mt-5 ">
+  <div class="card-header">Add New User</div>
+  <div class="card-body">
+    <form action="" class="form">
+      <label for="name" name="name">Name</label>
+      <input type="text" class="form-control mt-2" placeholder="Enter Your Name">
+
+      <label for="name" name="email">Email</label>
+      <input type="text" class="form-control mt-2" placeholder="Enter Your Email">
+
+      <label for="name" name="description">Description</label>
+      <input type="text" class="form-control mt-2" placeholder="Enter Your Description">
+
+      <input type="submit" class="btn btn-info mt-3" value="Add User">
+    </form>
+
+  </div>
+
+</div>
